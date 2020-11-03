9e16cb3fc75b8fb21b74d51f056b091e79ff8cba
yashrajpindi <69507774+yashrajpindi@users.noreply.github.com>
2020-11-03 19:41:00 +0530

    s

--- backyard/include/side_menu.php
+++ backyard/include/side_menu.php
@@ -38,6 +38,14 @@
                                     <li><a href="../picks/View-Picks">- View Picks</a></li>
                                 </ul>
                             </li>
+
+                            <li class="treeview">
+                                <a href="javascript:void(0)"><i class="zmdi zmdi-account-box-phone"></i> <span>Testimonial</span> <i class="fa fa-angle-right"></i></a>
+                                <ul class="treeview-menu">
+                                    <li><a href="../testimonials/Add-Testimonials">- Add Testimonial</a></li>
+                                    <li><a href="../testimonials/View-Testimonials">- View Testimonial</a></li>
+                                </ul>
+                            </li>
                            
                         </ul>
                     </nav>
