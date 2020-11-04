c8466f3e7843f64671703751986495051004d637
yashrajpindi <69507774+yashrajpindi@users.noreply.github.com>
2020-11-04 14:58:06 +0530

    dont touch testimonial

--- backyard/Testimonial/view_testimonial.php
+++ backyard/Testimonial/view_testimonial.php
@@ -80,7 +80,7 @@ function confirmationDelete(anchor)
 												<th>Client Name</th>
                                                 <th>Client Image</th>
 												<th>Message</th>
-                                                <th>Image</th>
+                                                
 												 <th>Edit</th>
 												<th>Remove</th>
                                             </tr>
@@ -99,7 +99,6 @@ function confirmationDelete(anchor)
 														<td><?php echo $cat['client_name']; ?></td>
                                                         <td><img style="width:100px;height:70px;" src="../images/client_image/<?php echo $cat['client_image']; ?>"></td>
 														<td><?php echo $cat['testimonial_msg']; ?></td>
-														<td><img style="width:100px;height:70px;" src="../images/testimonial_img/<?php echo $cat['testimonial_img']; ?>"></td>
 														
 													<td><a href="Edit-Testimonial/<?php echo $cat['testimonial_id']; ?>"><img style="height: 30px;width: 30px;"  src="../img/edit.png"></a></td>
 													<td><a onClick='javascript:confirmationDelete($(this));return false;' href="Delete-Testimonial/<?php echo $cat['testimonial_id'];?>"><img style="height: 30px;width: 30px;"  src="../img/delete.png"></a></td>
